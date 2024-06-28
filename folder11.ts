b78df181a8c2db689b9c0a6c59b57762f69d5e33
shamanaz12 <160182090+shamanaz12@users.noreply.github.com>
2024-06-28 12:14:00 -0700

    Add files via upload

---
+++ folder11.ts
@@ -0,0 +1,54 @@
+// let fruits: string[] = ["Mango", "Apple", "Banana", "Orange", "Pinapple"]
+
+// for(let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i]);
+// }
+
+
+
+
+
+// enum MENU {
+//     ROLL ="roll",
+//     BEEFROLL ="beefroll",
+//     MALAIBOTTI = "malaibotti",
+//     TIKKA = "tikka",
+// }
+// enum REVIEW {
+//     BAD,
+//     GOOD,
+//     DELICIOUS,
+// }
+// type Order ={
+//     order:MENU,
+//     payment:number,
+//     review:REVIEW
+// }
+// let hamzaOrder:Order={
+//     order: MENU.BEEFROLL,
+//     payment: 100,
+//     review:REVIEW.DELICIOUS
+// }
+// console.log(hamzaOrder);
+ 
+
+
+
+//let myName: any = "Ubaid"
+
+// console.log(myName.toUpperCase())
+
+//let age: unknown = 5;
+// console.log(typeof age)
+// if(typeof age === "number"){
+//     console.log(age * 2)
+// }else { 
+//     console.log("Incorrect data")
+// }
+
+// let person: unknown = 20
+
+// console.log((person as string).toUpperCase());
+
+// console.log(person.toUpperCase());
+ 
